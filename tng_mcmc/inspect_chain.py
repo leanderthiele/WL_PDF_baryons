711fa36ba405e1b2aa26bd00a884f40a462fb203
Leander Thiele <lthiele@princeton.edu>
2022-08-26 13:23:17 -0400

    small fixes in inspect_chain.py make it work

--- tng_mcmc/inspect_chain.py
+++ tng_mcmc/inspect_chain.py
@@ -25,21 +25,23 @@ with h5py.File(chain_fname, 'r') as f :
 # get the posterior visually
 reader = emcee.backends.HDFBackend(chain_fname, read_only=True)
 flat_samples = reader.get_chain(flat=True)
-corner_fig = corner.corner(samples, labels=param_names, plot_datapoints=True)
-corner.savefig('posterior_kappamin%.3f.pdf'%KAPPA_MIN)
+corner_fig = corner.corner(flat_samples, labels=param_names, plot_datapoints=True)
+corner_fig.savefig('posterior_kappamin%.3f.pdf'%KAPPA_MIN)
 
 # get the best fit
 nprocs = len(glob('%s/info_*.dat'%OUT_BASE))
+print('Found output from %d processes'%nprocs)
 min_chisq = np.inf
 best_proc = None
 best_idx = None
-for ii in range(1, procs) : # process 0 is not contibuting!
-    chisq = np.loadtxt('info_%d.dat'%ii, usecols=(0,))
+for ii in range(1, nprocs) : # process 0 is not contibuting!
+    chisq = np.loadtxt('%s/info_%d.dat'%(OUT_BASE, ii), usecols=(0,))
     if np.min(chisq) < min_chisq :
         best_proc = ii
         best_idx = np.argmin(chisq)
         min_chisq = np.min(chisq)
-theory_dmo_bf, theory_hydro_bf = np.loadtxt('sample_%d_%d'%(best_proc, best_idx))
+print('Best fit chisquared = %g'%min_chisq)
+theory_dmo_bf, theory_hydro_bf = np.loadtxt('%s/sample_%d_%d'%(OUT_BASE, best_proc, best_idx), unpack=True)
 theory_res_bf = 2.0 * (theory_hydro_bf-theory_dmo_bf) / (theory_hydro_bf+theory_dmo_bf)
 theory_kappa_edges = np.linspace(0.0, 0.2, num=33)
 theory_kappa = 0.5 * (theory_kappa_edges[1:] + theory_kappa_edges[:-1])
@@ -48,4 +50,5 @@ ax_obs.plot(target_kappa, target_res, label='target')
 ax_obs.plot(theory_kappa, theory_res_bf, label='theory best fit')
 ax_obs.set_xlabel('kappa')
 ax_obs.set_ylabel('2(hydro-DMO)/(hydro+DMO)')
+ax_obs.legend(loc='upper left')
 fig_obs.savefig('bestfit_kappamin%.3f.pdf'%KAPPA_MIN, bbox_inches='tight')
